37f02814100e3ba7ac11569326fc87e9d792abe5
imthiyazmuhammed <imthiyaz.muhd@gmail.com>
2021-05-22 00:39:30 +0530

    checkout page and authentication with next js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,23 @@ import {
 	ShoppingCartIcon,
 	shoppingIcon,
 } from '@heroicons/react/outline';
+import { signIn, signOut, useSession } from 'next-auth/client';
+import { useRouter } from 'next/router';
+import { useSelector } from 'react-redux';
+import { selectItems } from '../slices/basketSlice';
 
 function Header() {
+	const [session] = useSession();
+	const router = useRouter();
+	const items = useSelector(selectItems);
+
 	return (
 		<header>
 			{/* top nav */}
 			<div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
 				<div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
 					<Image
+						onClick={() => router.push('/')}
 						src="https://links.papareact.com/f90"
 						width={150}
 						height={40}
@@ -29,17 +38,19 @@ function Header() {
 					<SearchIcon className="h-12 p-4" />
 				</div>
 				<div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
-					<div className="link">
-						<p>Hello Imthiyaz</p>
+					<div onClick={!session ? signIn : signOut} className="link">
+						<p> {session ? `Hello, ${session.user.name}` : 'SignIn'} </p>
 						<p className="font-extrabold md:text-sm">Account & list</p>
 					</div>
 					<div className="link">
 						<p>Returns </p>
 						<p className="font-extrabold md:text-sm">& Orders</p>
 					</div>
-					<div className="relative link flex items-center ">
+					<div
+						onClick={() => router.push('/Checkout')}
+						className="relative link flex items-center ">
 						<span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
-							0
+							{items.length}
 						</span>
 						<ShoppingCartIcon className="h-10" />
 						<p className="hidden md:inline mt-2 font-extrabold md:text-sm ">
--- src/components/Product.js
+++ src/components/Product.js
@@ -2,13 +2,30 @@ import { StarIcon } from '@heroicons/react/solid';
 import Image from 'next/image';
 import { useState } from 'react';
 import Currency from 'react-currency-formatter';
+import { useDispatch } from 'react-redux';
+import { addToBasket } from '../slices/basketSlice';
 const MAX_RATING = 5;
 const MIN_RATING = 1;
+
 function Product({ id, title, price, description, category, image }) {
+	const dispatch = useDispatch();
 	const [rating] = useState(
 		Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
 	);
 	const [hasPrime] = useState(Math.random() < 0.5);
+	const addItemToBasket = () => {
+		const products = {
+			id,
+			title,
+			description,
+			price,
+			category,
+			image,
+			hasPrime,
+		};
+		///		shooting product to reedux store
+		dispatch(addToBasket(products));
+	};
 	return (
 		<div className="relative flex flex-col m-5 bg-white z-30 p-10">
 			<p className="absolute top-2 right-2 text-xs itlaic text-gray-400">
@@ -33,7 +50,9 @@ function Product({ id, title, price, description, category, image }) {
 					<p className="text-xs text-gray-500">Free same day delivery</p>
 				</div>
 			)}
-			<button className="mt-auto button">Add to Basket</button>
+			<button onClick={addItemToBasket} className="mt-auto button">
+				Add to Basket
+			</button>
 		</div>
 	);
 }
