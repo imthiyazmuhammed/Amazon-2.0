3bc953704f1e1095b5fcebc2370e853e8c60e9b7
imthiyazmuhammed <imthiyaz.muhd@gmail.com>
2021-05-22 11:44:44 +0530

    kuttys

--- src/components/Header.js
+++ src/components/Header.js
@@ -49,7 +49,7 @@ function Header() {
 					<div
 						onClick={() => router.push('/checkout')}
 						className="relative link flex items-center ">
-						<span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
+						<span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">{/*  */}
 							{items.length}
 						</span>
 						<ShoppingCartIcon className="h-10" />
